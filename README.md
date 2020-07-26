598ea8510020ec339f0e0ad4c72da125d593a293
Andy-0414 <pjh8667@naver.com>
2020-07-26 22:40:57 +0900

    Update lib settings

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Node.js + Express + MongoDB + Typescript 서버 템플릿
 
 개발중...
 
+# Functions
+
+-   JWT + AccessToken, RefreshToken 을 사용한 로그인 시스템
+
 # Convention
 
-- ES6, Typescript
-- 변수명은 camelCase, 모듈명은 PascalCase
+-   ES6, Typescript
+-   변수명은 camelCase, 모듈명은 PascalCase
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("Hello World")
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+
+import "dotenv/config";
+
+const app: express.Application = express(); // 서버 객체
+const port = process.env.PORT || 3000;
+
+app.use(morgan("dev")); // 서비스 시 로그 출력
+
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+); // CORS 설정 미들웨어
+app.use(helmet()); // 보안 미들웨어
+app.use(compression()); // 데이터 압축 미들웨어
+
+app.use(express.static("public")); // public 폴더의 파일을 제공함
+app.use(express.urlencoded({ limit: "20mb", extended: true })); // urlencode 지원
+app.use(express.json({ limit: "20mb" })); // json 지원
+
+const server = app.listen(port, () => {
+	// 서버가 열렸을 시 콜백
+});
+
+export default app;
