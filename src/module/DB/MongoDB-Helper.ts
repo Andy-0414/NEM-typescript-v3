b4617276daca7262e0f994af31aa94ceca148269
Andy-0414 <pjh8667@naver.com>
2020-09-18 22:32:39 +0900

    Update MognoDB

---
+++ src/module/DB/MongoDB-Helper.ts
@@ -0,0 +1,46 @@
+import Mongoose from "mongoose";
+import Log from "../Log";
+
+/**
+ * @description Mongo DB 관리 클래스
+ */
+class MongoDBHelper {
+	private db: Mongoose.Connection;
+	public isDatabaseConnect: boolean = false;
+
+	public readonly ENV: string = process.env.NODE_ENV || "development"; // 개발 환경
+	public readonly DB_URI: string = process.env.DB_URI || process.env.MONGODB_URI || "mongodb://localhost/NEM-TEMPLATE-V2"; // DB URL
+	/**
+	 * @description MongoDB 활성화
+	 * @param {string}url MongoDB URL
+	 */
+	public init(url?: string): void {
+		this.db = Mongoose.connection;
+
+		// 접속 실패 시
+		this.db.on("error", () => {
+			Log.e("Mongo DB connected fail");
+			Log.e("Server stop");
+			this.isDatabaseConnect = false;
+			process.exit();
+		});
+
+		// 접속 성공 시
+		this.db.once("open", () => {
+			Log.c("Mongo DB connected");
+			this.isDatabaseConnect = true;
+		});
+
+		Mongoose.set("useCreateIndex", true);
+		Mongoose.set("useUnifiedTopology", true);
+		Mongoose.connect(url || this.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+	}
+	/**
+	 * @description DB 객체 반환
+	 * @returns {Mongoose.Connection} DB 객체
+	 */
+	public getDB(): Mongoose.Connection {
+		return this.db;
+	}
+}
+export default new MongoDBHelper();
