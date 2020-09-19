df52d39edbf62651669fa819e5ce220f42565260
Andy-0414 <pjh8667@naver.com>
2020-09-20 00:33:19 +0900

    Update remark

--- src/schema/User.ts
+++ src/schema/User.ts
@@ -1,28 +1,24 @@
 import { Schema, model, Model, Document } from "mongoose";
 
 export interface IUser {
-	userID: string;
-	password: string;
-	loginType?: string;
-	username?: string;
-	email?: string;
-	salt?: string;
-	imgPath?: string;
-	lastLoginTime?: Date;
-	createdTime?: Date;
-	isAdmin?: boolean;
+	loginType: string; // 로그인 타입 (카카오 등)
+	username: string; // 유저 이름
+	email: string; // 이메일 (고유)
+	password: string; // 비밀번호
+	salt: string; // 비밀번호 키
+	profileImage: string; // 프로필 이미지 경로
+	lastLoginAt: Date; // 최근 로그인 시간
+	createAt: Date; // 회원가입 시간
 }
 export const UserSchema: Schema = new Schema({
 	loginType: { type: String, default: "local" },
 	username: { type: String, default: "User" },
-	email: { type: String, default: "" },
-	userID: { type: String, required: true, unique: true },
+	email: { type: String, default: "", required: true, unique: true },
 	password: { type: String, required: true, select: false },
 	salt: { type: String, default: process.env.SECRET_KEY || "SECRET", select: false },
-	imgPath: { type: String, default: "" },
-	lastLoginTime: { type: Date, default: Date.now },
-	createdTime: { type: Date, default: Date.now },
-	isAdmin: { type: Boolean, default: false },
+	profileImage: { type: String, default: "" },
+	lastLoginAt: { type: Date, default: Date.now },
+	createAt: { type: Date, default: Date.now },
 });
 
 /**
