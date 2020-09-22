3114e83fd1ddb9ef1e292326665431baae336d24
Andy-0414 <pjh8667@naver.com>
2020-09-22 19:34:15 +0900

    Update auto router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import RouterManager, { RouterPath } from "../module/Router-Manager";
 
 const router = Router();
 
-// TODO: 라우터 자동 검색
+// 라우터 자동 검색
+RouterManager.getRouters().forEach((rp: RouterPath) => {
+	router.use(rp.path, rp.router);
+});
 
 export default router;
