d3ece0fffe8bef857acb686b3fb7e634a87be580
Andy-0414 <pjh8667@naver.com>
2020-09-24 12:13:40 +0900

    Update router connect

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import morgan from "morgan";
 
 import "dotenv/config";
 import Log from "./module/Log";
+import Router from "./router";
 
 const app: express.Application = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +20,8 @@ app.use(compression()); // 데이터 압축 미들웨어
 app.use(express.static("public")); // public 폴더의 파일을 제공함
 app.use(express.urlencoded({ limit: "20mb", extended: true })); // urlencode 지원
 app.use(express.json({ limit: "20mb" })); // json 지원
+
+app.use(Router); // 라우터 연결
 
 const server = app.listen(port, () => {
 	Log.i(`PORT : ${port}`);
